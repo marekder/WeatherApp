d0c59275110fec486812b9cba9101d1da6d2bc5e
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2022-12-13 21:16:18 +0100

    20221213 - added js

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 import './sass/main.scss'
-import AOS from 'aos'
-AOS.init()
+
+let cityForm = document.querySelector('.weather__form')
+let cityInput = document.querySelector('.weather__city')
+
+cityForm.addEventListener('submit', (event) => {
+    let city = cityInput.value
+    // console.log(city)
+    if (city.length <= 3) {
+        cityInput.classList.add('weather__city--error')
+    } else {
+        cityInput.classList.remove('weather__city--error')
+    }
+
+    event.preventDefault()
+})
+
+cityInput.addEventListener('keyup', () => {
+    let city = cityInput.value
+    if (city.length <= 3) {
+        cityInput.classList.add('weather__city--error')
+    } else {
+        cityInput.classList.remove('weather__city--error')
+    }
+})
