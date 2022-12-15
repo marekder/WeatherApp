98d8b3a9d1ceeae46bfeafce24e77f31254354ab
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2022-12-15 18:56:56 +0100

    20221215 - api

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './sass/main.scss'
 
 let cityForm = document.querySelector('.weather__form')
 let cityInput = document.querySelector('.weather__city')
+let APIURL = 'http://api.weatherapi.com/v1/current.json?key=d65612371be147b590e171953221512&aqi=yes&q='
 
 cityForm.addEventListener('submit', (event) => {
     let city = cityInput.value
@@ -9,7 +10,12 @@ cityForm.addEventListener('submit', (event) => {
     if (city.length <= 3) {
         cityInput.classList.add('weather__city--error')
     } else {
+
         cityInput.classList.remove('weather__city--error')
+
+        let APIURLWITHCITY = APIURL + city
+        fetch(APIURLWITHCITY)
+        // console.log(APIURLWITHCITY)
     }
 
     event.preventDefault()
